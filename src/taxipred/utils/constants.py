a008973b72a21254cdf4d11485dface8b3d25604
Daniela Algerydh <danielaalgerydh@gmail.com>
2026-01-12 16:42:46 +0100

    Model dump

--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -4,3 +4,6 @@ PROJECT_ROOT = Path(__file__).parents[3].resolve()
 DATA_PATH = PROJECT_ROOT / "data"
 CLEANED_DATA = DATA_PATH / "processed"
 TAXI_CSV_PATH = DATA_PATH / "raw" / "taxi_trip_pricing.csv"
+
+MODEL_PATH = Path(__file__).parents[3].resolve() / "models"
+MODEL = MODEL_PATH / "taxi_price_predictor"

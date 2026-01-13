70bf58a0739ce927f4b02116705fc84691b1f17e
Daniela Algerydh <danielaalgerydh@gmail.com>
2026-01-13 11:29:18 +0100

    Change constants and small edit in notebooks

--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -8,4 +8,4 @@ TAXI_CSV_RAW = DATA_PATH / "raw" / "taxi_trip_pricing.csv"
 TAXI_CSV_CLEANED = CLEANED_DATA / "taxi_fillednan_all_columns.csv"
 
 MODEL_PATH = Path(__file__).parents[3].resolve() / "models"
-MODEL = MODEL_PATH / "taxi_price_predictor"
+MODEL = MODEL_PATH / "taxi_price_predictor.joblib"

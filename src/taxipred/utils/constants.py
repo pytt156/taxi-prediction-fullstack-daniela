75f8354acb0f0f0be80cfa6102d2f9749f5c8161
Daniela Algerydh <danielaalgerydh@gmail.com>
2026-01-16 09:04:20 +0100

    Improve data cleaning

--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -5,7 +5,6 @@ DATA_PATH = PROJECT_ROOT / "data"
 CLEANED_DATA = DATA_PATH / "processed"
 
 TAXI_CSV_RAW = DATA_PATH / "raw" / "taxi_trip_pricing.csv"
-TAXI_CSV_CLEANED = CLEANED_DATA / "taxi_fillednan_all_columns.csv"
 
 MODEL_PATH = Path(__file__).parents[3].resolve() / "models"
 MODEL = MODEL_PATH / "taxi_price_predictor.joblib"

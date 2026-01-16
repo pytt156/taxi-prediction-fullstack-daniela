3f5af046b353a52f9363fafeae4e99d13d2e7c1b
Daniela Algerydh <danielaalgerydh@gmail.com>
2026-01-16 14:09:23 +0100

    Test frontend predicts

--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -5,6 +5,7 @@ DATA_PATH = PROJECT_ROOT / "data"
 CLEANED_DATA = DATA_PATH / "processed"
 
 TAXI_CSV_RAW = DATA_PATH / "raw" / "taxi_trip_pricing.csv"
+TAXI_CSV_CLEANED = CLEANED_DATA / "taxi_prices_cleaned.csv"
 
 MODEL_PATH = Path(__file__).parents[3].resolve() / "models"
 MODEL = MODEL_PATH / "taxi_price_predictor.joblib"
